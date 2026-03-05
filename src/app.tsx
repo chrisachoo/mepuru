9283de6eff1f1d0f0a5993d7226b40d1066563a8
chrisachoo <mabs.chris@outlook.com>
2026-03-05 11:51:10 +0200

    Refactor App component to use a separate Head component for meta tags and suspense handling

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,24 @@
-import { MetaProvider, Title } from "@solidjs/meta";
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import "./app.css";
+import type { ParentProps } from "solid-js"
+
+import { MetaProvider, Title } from "@solidjs/meta"
+import { Router } from "@solidjs/router"
+import { FileRoutes } from "@solidjs/start/router"
+import { Suspense } from "solid-js"
+import "./app.css"
+
+function Head(props: Readonly<ParentProps>) {
+	return (
+		<MetaProvider>
+			<Title>Mepuru — Simple reactive UI for Solid</Title>
+			<Suspense>{props.children}</Suspense>
+		</MetaProvider>
+	)
+}
 
 export default function App() {
-  return (
-    <Router
-      root={props => (
-        <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	return (
+		<Router root={props => <Head>{props.children}</Head>}>
+			<FileRoutes />
+		</Router>
+	)
 }
