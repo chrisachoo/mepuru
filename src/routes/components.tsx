82de629bd5a74521e8427a6fb06eadc34b684d0c
chrisachoo <mabs.chris@outlook.com>
2026-03-16 21:43:23 +0200

    Update documents and components route layout

--- src/routes/components.tsx
+++ src/routes/components.tsx
@@ -2,11 +2,11 @@ import type { RouteSectionProps } from "@solidjs/router"
 import { Link, Meta, MetaProvider, Title } from "@solidjs/meta"
 import { RoutesLayout } from "~/components/layout/routes-layout"
 
-export default function DocsLayout(props: Readonly<RouteSectionProps>) {
+export default function ComponentsLayout(props: Readonly<RouteSectionProps>) {
 	return (
 		<MetaProvider>
 			<div>
-				<Title>Documents</Title>
+				<Title>Components</Title>
 				<Link
 					rel="canonical"
 					href="http://solidjs.com/"
